20d910096d964c58a7a798a337809ff1b3c367d1
Emerson Siega <emersonsiega@hotmail.com>
2023-08-03 08:50:18 -0300

    feat: Add UITableView and connect to the ViewController

--- MagicTravel/ViewController.swift
+++ MagicTravel/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IB -> Interface builder
+    // It's a connection to the TableView at the UI
+    //
+    // To connect with the UI:
+    // - Right click on TableView at the UI
+    // - New Referencing Outlet
+    // - Drag over the ViewController
+    // - Select "countriesTableView"
+    @IBOutlet var countriesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
