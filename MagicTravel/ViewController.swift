696467026ed1908496c60c6e7362c874828472b1
Emerson Siega <emerson.siega@genial.com.vc>
2023-08-03 08:27:26 -0300

    Initial Commit

---
+++ MagicTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTravel
+//
+//  Created by emersonsiega on 03/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
