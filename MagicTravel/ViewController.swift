8e0f5d41af99da6e779a91878b9f12bf5386552b
Emerson Siega <emersonsiega@hotmail.com>
2023-08-03 21:49:26 -0300

    feat: Add UITableViewDatasource and initialize it

--- MagicTravel/ViewController.swift
+++ MagicTravel/ViewController.swift
@@ -19,11 +19,48 @@ class ViewController: UIViewController {
     // - Select "countriesTableView"
     @IBOutlet var countriesTableView: UITableView!
     
+    struct Constants {
+        static let cellIdentifier = "CountryCell"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countriesTableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: Constants.cellIdentifier
+        )
     }
+}
 
-
+// Adds a datasourte to the UITableView
+// As the "countriesTableView" needs to be connected to the outlet in the UI
+//
+// To connect with the UI:
+// - Right click on TableView at the UI
+// - Outlets -> Datasource
+// - Drag over the ViewController
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // dequeueReusableCell -> will mount the necessary cells in the UI and,
+        // if necessary (a.k.a. user scrolls), will reuse the cells to display different data
+        let tableViewCell = tableView.dequeueReusableCell(
+            withIdentifier: Constants.cellIdentifier,
+            for: indexPath
+        )
+        
+        
+        return tableViewCell
+    }
+    
+    
 }
 
